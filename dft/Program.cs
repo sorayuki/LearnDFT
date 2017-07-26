a9d299c30f5aaf265ec4b87bceb8e4e05b7993cd
SoraYuki <sorayuki@live.com>
2017-07-26 15:06:47 +0800

    Initial commit

---
+++ dft/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexVector vec = new ComplexVector(16);
+            for(int i = 0; i < 16; ++i)
+            {
+                ComplexVector tmp = new ComplexVector(16);
+                tmp.SetFourierSequence(i);
+                tmp = tmp * (new Complex(i));
+                vec = vec + tmp;
+            }
+
+            for (int i = 0; i < 16; ++i)
+            {
+                ComplexVector tmp = new ComplexVector(16);
+                tmp.SetFourierSequence(i);
+
+                Complex x = vec * tmp;
+                Complex k = tmp * tmp;
+                Console.WriteLine("{0}", x / k);
+            }
+        }
+    }
+}
